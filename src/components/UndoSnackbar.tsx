6d7873a12233ab64412371056df6979fe6718e6b
shivam <shivamtech181@gmail.com>
2025-11-13 19:22:16 +0530

    Fix Add onExited callback to UndoSnackbar component

--- src/components/UndoSnackbar.tsx
+++ src/components/UndoSnackbar.tsx
@@ -4,9 +4,10 @@ interface Props {
   open: boolean;
   onClose: () => void;
   onUndo: () => void;
+  onExited?: () => void;
 }
 
-export default function UndoSnackbar({ open, onClose, onUndo }: Props) {
+export default function UndoSnackbar({ open, onClose, onUndo, onExited }: Props) {
   return (
     <Snackbar
       open={open}
@@ -15,6 +16,7 @@ export default function UndoSnackbar({ open, onClose, onUndo }: Props) {
       message="Task deleted"
       action={<Button color="secondary" size="small" onClick={onUndo}>Undo</Button>}
       anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      TransitionProps={{ onExited: onExited }}
     />
   );
 }
